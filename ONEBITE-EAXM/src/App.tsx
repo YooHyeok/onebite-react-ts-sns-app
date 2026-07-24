c8634938cde309c59a72d969e6a1285c5c475d43
유재혁 <yjk7454@naver.com>
2026-07-25 00:07:39 +0900

    feat: `App.tsx` - shadcn utils.ts cn유틸 조건부 Tailwind 클래스 적용 예제

--- ONEBITE-EAXM/src/App.tsx
+++ ONEBITE-EAXM/src/App.tsx
@@ -3,14 +3,20 @@ import Tailwind from "./components/tailwind/Tailwind";
 
 import { Button } from "@/components/ui/button";
 
+import { cn } from "@/lib/utils";
+const isActive = true;
+
 function App() {
   return (
     <div>
       {/* <Tailwind /> */}
       {/* <Button>버튼!</Button> */}
-      <div className="text-primary">Primary</div>
+      {/* <div className="text-primary">Primary</div>
       <div className="text-muted">Muted</div>
-      <div className="text-destructive">Destructive</div>
+      <div className="text-destructive">Destructive</div> */}
+      <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
+        isActive
+      </div>
     </div>
   );
 }
