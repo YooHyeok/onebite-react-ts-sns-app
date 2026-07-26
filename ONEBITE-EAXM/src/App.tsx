83e803c1e3a34c73f5b73ad8fbcb52ef4745c812
유재혁 <yjk7454@naver.com>
2026-07-26 20:18:13 +0900

    feat: `App.tsx` - shadcn input.tsx 컴포넌트 적용 예제

--- ONEBITE-EAXM/src/App.tsx
+++ ONEBITE-EAXM/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Tailwind from "./components/tailwind/Tailwind";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 
 const isActive = true;
@@ -16,11 +17,14 @@ function App() {
       {/* <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
         isActive
         </div> */}
-      <Button variant={"destructive"}>버튼</Button>
-      <Button variant={"ghost"}>버튼</Button>
-      <Button variant={"link"}>버튼</Button>
-      <Button variant={"outline"}>버튼</Button>
-      <Button variant={"secondary"}>버튼</Button>
+      <div className="p-5">
+        <Button variant={"destructive"}>버튼</Button>
+        <Button variant={"ghost"}>버튼</Button>
+        <Button variant={"link"}>버튼</Button>
+        <Button variant={"outline"}>버튼</Button>
+        <Button variant={"secondary"}>버튼</Button>
+        <Input value={"기본값"} placeholder="입력 ..." />
+      </div>
     </div>
   );
 }
