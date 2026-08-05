dac582fd05eb35c29f50876642e680b9177ce673
유재혁 <yjk7454@naver.com>
2026-08-05 20:13:55 +0900

    feat: Outlet과 중첩 라우팅을 활용한 공통 헤더 레이아웃 구성 예제

--- ONEBITE-EAXM/src/App.tsx
+++ ONEBITE-EAXM/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
 import Tailwind from "@/components/tailwind/Tailwind";
 import Shadcn from "@/components/Shadcn";
-import { Route, Routes } from "react-router";
+import { Outlet, Route, Routes } from "react-router";
 import SignInPage from "@/pages/sign-in-page";
 import SignUpPage from "@/pages/sign-up-page";
+
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -15,8 +24,12 @@ function App() {
       <Route path="/" element={<div>HOME</div>} />
       {/* <Route path="/sign-in" element={<div>SignIn</div>} />
       <Route path="/sign-up" element={<div>SignUp</div>} /> */}
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
+      {/* <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} /> */}
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
     </Routes>
   );
 }
