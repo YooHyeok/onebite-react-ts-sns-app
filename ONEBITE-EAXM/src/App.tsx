4d5a8e199fbade52fcafbc08e6d53198aa8949dc
유재혁 <yjk7454@naver.com>
2026-08-05 21:59:09 +0900

    fix: `App.tsx` - react-router IndexPage 컴포넌트 라우트 등록 누락 적용

--- ONEBITE-EAXM/src/App.tsx
+++ ONEBITE-EAXM/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Tailwind from "@/components/tailwind/Tailwind";
 import Shadcn from "@/components/Shadcn";
 import { Outlet, Route, Routes } from "react-router";
+import IndexPage from "@/pages/index-page";
 import SignInPage from "@/pages/sign-in-page";
 import SignUpPage from "@/pages/sign-up-page";
 
@@ -21,9 +22,10 @@ function App() {
       <Shadcn />
     </div> */
     <Routes>
-      <Route path="/" element={<div>HOME</div>} />
+      {/* <Route path="/" element={<div>HOME</div>} /> */}
       {/* <Route path="/sign-in" element={<div>SignIn</div>} />
       <Route path="/sign-up" element={<div>SignUp</div>} /> */}
+      <Route path="/" element={<IndexPage />} />
       {/* <Route path="/sign-in" element={<SignInPage />} />
       <Route path="/sign-up" element={<SignUpPage />} /> */}
       <Route element={<AuthLayout />}>
