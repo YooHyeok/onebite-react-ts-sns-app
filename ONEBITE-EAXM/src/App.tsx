3df54145f5bfc3f52d1c591d783ee56b2b5fa723
유재혁 <yjk7454@naver.com>
2026-08-05 20:06:05 +0900

    feat: `App.tsx` - react-router routes 실제 컴포넌트 적용 예제

--- ONEBITE-EAXM/src/App.tsx
+++ ONEBITE-EAXM/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Tailwind from "@/components/tailwind/Tailwind";
 import Shadcn from "@/components/Shadcn";
 import { Route, Routes } from "react-router";
+import SignInPage from "@/pages/sign-in-page";
+import SignUpPage from "@/pages/sign-up-page";
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
     </div> */
     <Routes>
       <Route path="/" element={<div>HOME</div>} />
-      <Route path="/sign-in" element={<div>SignIn</div>} />
-      <Route path="/sign-up" element={<div>SignUp</div>} />
+      {/* <Route path="/sign-in" element={<div>SignIn</div>} />
+      <Route path="/sign-up" element={<div>SignUp</div>} /> */}
+      <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
     </Routes>
   );
 }
