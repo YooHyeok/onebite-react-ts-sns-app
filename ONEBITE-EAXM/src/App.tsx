88647f3c4fd3578372672099879f7d80f9b2d3ba
유재혁 <yjk7454@naver.com>
2026-08-07 22:01:39 +0900

    feat: `pages/counter-page.tsx` - zustand count store 모듈 적용

--- ONEBITE-EAXM/src/App.tsx
+++ ONEBITE-EAXM/src/App.tsx
@@ -5,6 +5,7 @@ import { Outlet, Route, Routes } from "react-router";
 import IndexPage from "@/pages/index-page";
 import SignInPage from "@/pages/sign-in-page";
 import SignUpPage from "@/pages/sign-up-page";
+import CounterPage from "@/pages/counter-page";
 
 function AuthLayout() {
   return (
@@ -28,6 +29,7 @@ function App() {
       <Route path="/" element={<IndexPage />} />
       {/* <Route path="/sign-in" element={<SignInPage />} />
       <Route path="/sign-up" element={<SignUpPage />} /> */}
+      <Route path="/counter" element={<CounterPage />}></Route>
       <Route element={<AuthLayout />}>
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
