10eab2fed372e276e58c7a31e0358106a6e7bcf1
유재혁 <yjk7454@naver.com>
2026-08-17 21:29:09 +0900

    feat: `zustand` - 투두리스트 기능구현 todos store 모듈 컴포넌트 적용

--- ONEBITE-EAXM/src/pages/todo-list/todo-item.tsx
+++ ONEBITE-EAXM/src/pages/todo-list/todo-item.tsx
@@ -1,16 +1,19 @@
 import { Button } from "@/components/ui/button";
+import type { Todo } from "@/types/todo-list";
+import { useDeleteTodo } from "@/store/todo-list/todos";
 
-export default function TodoItem({
-  id,
-  content,
-}: {
-  id: number;
-  content: string;
-}) {
+export default function TodoItem({ id, content }: Todo) {
+  const deleteTodo = useDeleteTodo();
+  console.count(`TodoItem ${id}`);
+  const handleDeleteClick = () => {
+    deleteTodo(id);
+  };
   return (
     <div className="flex items-center justify-between border p-2">
       {content}
-      <Button variant={"destructive"}>삭제</Button>
+      <Button onClick={handleDeleteClick} variant={"destructive"}>
+        삭제
+      </Button>
     </div>
   );
 }
