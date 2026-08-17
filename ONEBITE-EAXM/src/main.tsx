deefb5fc2ac63b5b0ae98f75da03efc06b76290e
유재혁 <yjk7454@naver.com>
2026-08-18 00:33:45 +0900

    feat: `main.tsx` - tanstack-query QueryClientProvider 컴포넌트 entry point 등록

--- ONEBITE-EAXM/src/main.tsx
+++ ONEBITE-EAXM/src/main.tsx
@@ -3,8 +3,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+/**
+ * TanstackQuery를 이용해 관리하는 모든 서버 상태를 보관하는 일종의 저장소 즉, 스토어
+ * API 요청의 응답값, 캐싱 값, 캐시 옵션들 등 서버 상태와 관련된 다양한 값들이 보관된다.  
+ */
+const queryClient = new QueryClient(); //
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
