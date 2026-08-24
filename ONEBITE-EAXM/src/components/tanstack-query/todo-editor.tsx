0728eae8bd3688068fb6ba4f35e1b52f792f98d7
유재혁 <yjk7454@naver.com>
2026-08-25 03:32:09 +0900

    refactor: `tanstack query` - todo-list todo 추가 useMutation 코드 모듈화

--- ONEBITE-EAXM/src/components/tanstack-query/todo-editor.tsx
+++ ONEBITE-EAXM/src/components/tanstack-query/todo-editor.tsx
@@ -1,30 +1,10 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useMutation } from "@tanstack/react-query";
-import { createTodo } from "@/api/create-todo";
+import { useCreateTodoMutation } from "@/hooks/mutations/use-create-todo-mutations";
 
 export default function TodoEditor() {
-  /**
-   * useMutation:
-   *              useQuery와는 다르게 컴포넌트가 마운트 되자마자 비동기 함수를 호출하지 않는다.
-   * mutate: 해당 함수 호출을 통해 mutationFn에 설정된 함수를 비동기 요청으로 실행하는 역할
-   *         mutate 호출로 수동으로 요청하는 이유는 useMutation이라는 훅으로 createTodo의 상태까지 관리하기 위함.
-   * isPending: mutate 호출을 톹한 비동기 요청의 로딩 상태
-   *
-   */
-  const { mutate, isPending } = useMutation({
-    mutationFn: createTodo,
-    /* 이벤트 핸들러 */
-    onMutate: /*요청 발송 시점*/ () => {},
-    onSettled: /*요청 종료 시점*/ () => {},
-    onSuccess: () => {
-      window.location.reload();
-    },
-    onError: (error) => {
-      window.alert(error.message);
-    },
-  });
+  const { mutate, isPending } = useCreateTodoMutation();
 
   const [content, setContent] = useState("");
   const handleAddClick = () => {
