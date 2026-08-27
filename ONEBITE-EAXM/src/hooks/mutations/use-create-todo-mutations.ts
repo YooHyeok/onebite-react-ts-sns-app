fcad98b4fbb6f3fd8f7284585653b4f3e3d575d0
유재혁 <yjk7454@naver.com>
2026-08-27 23:43:49 +0900

    refactor: `tanstack query` - 추가 후 목록 갱신방식 변경 (invalidateQueries → setQueryData)

--- ONEBITE-EAXM/src/hooks/mutations/use-create-todo-mutations.ts
+++ ONEBITE-EAXM/src/hooks/mutations/use-create-todo-mutations.ts
@@ -1,6 +1,7 @@
 import { createTodo } from "@/api/create-todo";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types/todo-list";
 
 /**
  * [useMutation]: 서버 상태 즉, 캐싱된 데이터를 생성,수정,삭제하는 등의 변경(Mutation) 작업을 처리할 때 사용하는 훅  
@@ -17,13 +18,25 @@ export function useCreateTodoMutation() {
     /* 이벤트 핸들러 */
     onMutate: /*요청 발송 시점*/ () => {},
     onSettled: /*요청 종료 시점*/ () => {},
-    onSuccess: () => {
+    onSuccess: (newTodo: Todo) => {
       /**
        * [invalidateQueries] todos 이름을 갖는 Tanstack Query 캐시 데이터를 무효화 한다.
        * queryClient: 서버 상태와 관련된 모든 데이터를 보관하는 저장소로 cache 데이터와 state들이 모두 보관되는 객체
        */
-      queryClient.invalidateQueries({
+      /* queryClient.invalidateQueries({
         queryKey: QUERY_KEYS.todo.list
+      }) */
+
+      /**
+       * 캐시 무효화 방식은 직관적이고 간단한 장점이 있지만,  
+       * 백엔드 서버에 저장된 데이터가 많아지면 많아질수록 리패칭 해야되는 데이터의 양이 계속 증가되므로  
+       * 서버에 부하가 발생할 수 있다.  
+       * 만약 리패칭해야 되는데이터의 양이 많아질 것 같은 경우 무효화 방식보다는 기존 캐시 저장소의 데이터를 즉시 업데이트 한다.  
+       * QueryClient 캐시 저장소와 json-server는 서로 독립적. json-server에 먼저 저장하고 캐시 저장소에도 갱신함으로써 바로 반영됨.
+       */
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if(!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo]
       })
     },
     onError: (error) => {
