2540473bd8aa733cc354d2b2e7b2f4addd7b967f
유재혁 <yjk7454@naver.com>
2026-08-29 19:27:48 +0900

    feat: `tanstack query` - 낙관적 업데이트 예외사항 3 - 백엔드 동기화를 위한 리패칭

--- ONEBITE-EAXM/src/hooks/mutations/use-update-todo.mutation.ts
+++ ONEBITE-EAXM/src/hooks/mutations/use-update-todo.mutation.ts
@@ -12,6 +12,7 @@ export default function useUpdateTodoMutation() {
       console.log("[onMutate] 낙관적 업데이트")
       /* 예외상황 2. 초기 요청에 대한 캐시 업데이트 이전에 캐시를 수정할경우, 초기 요청 완료시 수정된 캐시가 덮어씌워지게 되는 케이스 */
       await queryClient.cancelQueries({queryKey: QUERY_KEYS.todo.list})
+
       const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
       queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
         if (!prevTodos) return []; // 수정을 하려고 했는데 이전 배열이 유효하지 않으면 데이터가 없는것과 같음.
@@ -32,5 +33,12 @@ export default function useUpdateTodoMutation() {
 
       }
     },
+    /* 예외상황 3. 백엔드 데이터와 무결성이 깨지는 경우 케시 데이터를 무효화하여 리패칭 */
+    onSettled: () => { // 요청 종료시
+      console.log("[onSettled] 데이터 무효화 리패칭을 통한 서버 싱크 동기화")
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list
+      })
+    }
   })
 }
