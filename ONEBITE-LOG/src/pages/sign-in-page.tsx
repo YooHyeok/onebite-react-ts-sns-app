cc0cc39c35cba99bd101854b5a1b264df694cad4
유재혁 <yjk7454@naver.com>
2026-09-06 13:03:52 +0900

    feat: `sign-in-page.tsx` - 로그인 컴포넌트 기능 구현
    supabase 로그인 api 호출 모듈 tanstack query hook 연동

--- ONEBITE-LOG/src/pages/sign-in-page.tsx
+++ ONEBITE-LOG/src/pages/sign-in-page.tsx
@@ -1,17 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in";
+import { useState } from "react";
 import { Link } from "react-router";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const { mutate: signInWithPassword } = useSignInWithPassword();
+
+  const handleSignInWithPasswordClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+    signInWithPassword({ email, password });
+  };
   return (
     <div className="flex flex-col gap-8">
       <div className="text-xl font-bold">로그인</div>
       <div className="flex flex-col gap-2">
-        <Input className="py-6" type="email" placeholder="example@abc.com" />
-        <Input className="py-6" type="password" placeholder="password" />
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="py-6"
+          type="email"
+          placeholder="example@abc.com"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="py-6"
+          type="password"
+          placeholder="password"
+        />
       </div>
       <div>
-        <Button className="w-full">로그인</Button>
+        <Button onClick={handleSignInWithPasswordClick} className="w-full">
+          로그인
+        </Button>
       </div>
       <div>
         <Link className="text-muted-foreground hover:underline" to={"/sign-up"}>
