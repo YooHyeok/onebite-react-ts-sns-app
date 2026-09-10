9bdf2bad98a60a8801743bb08cda642ac3e1bd0e
유재혁 <yjk7454@naver.com>
2026-09-10 22:24:45 +0900

    feat: `App.tsx` - supabase 로그인 session 관리용 zustand store useSetSession 적용
    supabase 클라이언트의 auth.onAuthStateChange 이벤트 핸들러 내에서 초기화.
    useEffect 내부에 선언함으로써 컴포넌트가 마운트 되었을 때 1번만 이벤트 핸들러 등록(리랜더링 되더라도 등록되지 않음)
    
    만약 useEffect 외부에 그냥 선언할 경우 App 컴포넌트가 랜더링 될때마다 중첩으로 이벤트 핸들러가 등록되게 됨.

--- ONEBITE-LOG/src/App.tsx
+++ ONEBITE-LOG/src/App.tsx
@@ -1,5 +1,17 @@
 import RootRoute from "@/root-route";
+import supabase from "@/lib/supabase";
+import { useEffect } from "react";
+import { useSetSession } from "@/store/session";
 
 export default function App() {
+  const setSession = useSetSession();
+
+  useEffect(() => {
+    /* 컴포넌트가 마운트되었을때 session 정보 업데이트 감지 이벤트 핸들러 등록 */
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return <RootRoute />;
 }
