2d63e250eda2cf6ee86d0462310070cb4efc7189
유재혁 <yjk7454@naver.com>
2026-09-14 17:47:23 +0900

    feat:  `api/auth.ts` - 인증모듈 supabase 패스워드 초기화 resetPasswordForEmail api 함수 정의

--- ONEBITE-LOG/src/api/auth.ts
+++ ONEBITE-LOG/src/api/auth.ts
@@ -21,3 +21,17 @@ export async function signInWithOAuth(provider: Provider) {
   if (error) throw error
   return data;
 }
+
+export async function requestPasswordRestEmail(email: string) {
+  /**
+   * VITE_SUPABASE_URL/auth/v1/verify?toekn={value}&type=recovery&redirect_to={사용자에게 제공될 비밀번호 재설정 url}
+   * 
+   * supabase가 해당 url로 요청하기 전에 token을 생성한다.
+   * 실제 요청을 진행하며 해당 token값을 검증하여 유효하다면 redirect_to로 넘어온 주소로 리디렉션한다.
+   */
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${import.meta.env.VITE_PUBLIC_URL}/reset-password`
+  })
+  if (error) throw error
+  return data;
+}
