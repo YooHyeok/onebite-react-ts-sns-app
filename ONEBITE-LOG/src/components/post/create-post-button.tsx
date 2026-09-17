23f2c62c2f8fa5408b293ae5e1d8fc31c8908bea
유재혁 <yjk7454@naver.com>
2026-09-17 22:19:00 +0900

    feat: `components/post/create-post-button.tsx` - 포스트 추가 버튼에 모달 열기 액션 연동

--- ONEBITE-LOG/src/components/post/create-post-button.tsx
+++ ONEBITE-LOG/src/components/post/create-post-button.tsx
@@ -1,9 +1,12 @@
+import { useOpenPostEditorModal } from "@/store/post-editor-modal";
 import { PlusCircleIcon } from "lucide-react";
 
 export default function CreatePostButton() {
+  const openPostEditorModal = useOpenPostEditorModal();
 
   return (
     <div
+      onClick={() => openPostEditorModal()}
       className="bg-muted text-muted-foreground cursor-pointer rounded-xl px-6 py-4"
     >
       <div className="flex items-center justify-between">
